7f09e8aade0cb308ac248750c4dd94902d687532
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-07-06 11:57:30 +0530

    Initial commit

---
+++ industrial-safety-dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-cyan-400">
+          Industrial Safety Dashboard
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
